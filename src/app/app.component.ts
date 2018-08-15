6c4bc202d51df64a94f1e49f4ad3f5e88c89f3e8
Florian Bartkowiak <flo@iMac-de-Florian.local>
2018-08-15 17:23:10 +0200

    Dernier commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BlogExample';
+    posts= [
+    {
+      title: "Mon premier post",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      loveIts: 0 ,
+      created_at: new Date() 
+    },
+    {
+      title: "Mon deuxième post",
+      content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium",
+      loveIts: 0 ,
+      created_at: new Date()  
+    },
+    {
+      title: "Encore un post",
+      content: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum ",
+      loveIts: 0 ,
+      created_at: new Date()  
+    }
+  ];
+
+  constructor() {
+  }
 }
